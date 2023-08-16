e616fcfef37a58d1f032dce54d91126ed3469d73
zetLighter <127424545+zetLighter@users.noreply.github.com>
2023-08-16 21:26:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.textColor = '#FFFFFF'
+tg.MainButton.color = '2cab37';
+
+let item = '';
+
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+
+btn1.addEventListener('click', function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText('Вы выбрали первый товар.');
+		item = '1';
+		tg.MainButton.show();
+	}
+});
+btn2.addEventListener('click', function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText('Вы выбрали второй товар.');
+		item = '2';
+		tg.MainButton.show();
+	}
+});
+btn3.addEventListener('click', function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText('Вы выбрали третий товар.');
+		item = '3';
+		tg.MainButton.show();
+	}
+});
+btn4.addEventListener('click', function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText('Вы выбрали четвертый товар.');
+		item = '4';
+		tg.MainButton.show();
+	}
+
+});
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+	tg.sendData(item)
+});
+
+let usercard = document.getElementById('usercard');
+let paragraph = document.createElement('p');
+paragraph.innerText = '${tg.initDataUnsafe.first_name} ${tg.initDataUnsafe.last_name}';
+usercard.appendChild(paragraph);
+
